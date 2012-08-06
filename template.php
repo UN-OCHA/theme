7fa8f4cc6f0c49f2429abc7b25497df0a4740800
Guillaume Viguier-Just <guillaume@viguierjust.com>
2012-08-06 15:40:56 +0200

    Updating theme for new home page layout

--- template.php
+++ template.php
@@ -235,13 +235,13 @@ function humanitarianresponse_preprocess_block(&$vars) {
   if ($vars['block']->module == 'views') {
     switch ($vars['block']->delta) {
       case 'documents-documents_home_rep':
-        $vars['classes'] .= 'grid-4 alpha';
+        array_push($vars['classes_array'], 'grid-4', 'alpha');
         break;
       case 'maps_view-visuals_home':
-        $vars['classes'] .= 'grid-4';
+        array_push($vars['classes_array'], 'grid-4');
         break;
       case 'documents-documents_home_feat':
-        $vars['classes'] .= 'grid-4 omega';
+        array_push($vars['classes_array'], 'grid-4', 'omega');
         break;
     }
   }
