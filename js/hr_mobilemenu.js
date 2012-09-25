9e3568d23b70fb430f95fa4b0cd6b6ecc187d741
Guillaume Viguier-Just <guillaume@viguierjust.com>
2012-09-25 16:06:26 +0200

    Search box

--- js/hr_mobilemenu.js
+++ js/hr_mobilemenu.js
@@ -4,4 +4,7 @@ jQuery(document).ready(function($) {
   $("<select id=\"main-menu-mobile\" />").appendTo("#region-menu .region-inner");
   $("#main-menu-mobile").mobileMenu({ ulsource: "#block-superfish-1 .content ul", maxLevel : 4 });
   $('input[name="search_api_views_fulltext"]').val('Search');
+  $('input[name="search_api_views_fulltext"]').focus(function() {
+    $(this).val('');
+  });
 });
