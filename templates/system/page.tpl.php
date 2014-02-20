0a2a4097030fa298fa9df5b389f95a47085a5f27
MaximeFC <maxime.gaul@feelandclic.com>
2014-02-20 10:15:18 +0100

    Update

--- templates/system/page.tpl.php
+++ templates/system/page.tpl.php
@@ -10,95 +10,97 @@
  * @see html.tpl.php
  */
 ?>
-<header id="header" class="header" role="header">
-  <div class="container">
-    <div id="top">
-      <nav class="navbar navbar-default">
-          <!-- Brand and toggle get grouped for better mobile display -->
-          <div class="navbar-header top">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-          </div>
-
-          <div class="collapse navbar-collapse">
-            <ul class="nav nav-tabs navbar-left">
-              <li class="dropdown">
-                <a class="dropdown-toggle" data-toggle="dropdown" href="#"> HR <span class="caret"></span></a>
-                <ul class="dropdown-menu">
-                  <?php print render($main_menu_dropdown); ?>
-                </ul>
-              </li>
-              <?php if ($og_group): ?>
-                <li class="active"><?php print $og_group; ?></li>
-              <?php endif; ?>
-            </ul>
-            <div class="navbar-right">
-              <?php print render($page['top']); ?>
-            </div>
-          </div><!-- .navbar-collapse -->
-      </nav>
-    </div><!-- #top -->
-  </div><!-- .container-fluid -->
-  <div class="container header">
-    <div id="branding">
-        <?php if ($logo): ?>
-          <a href="<?php print $front_page; ?>" id="logo">
-            <img src="<?php print $logo; ?>" alt="Humanitarianresponse Logo" />
-          </a>
-        <?php endif; ?>
-        <?php print render($page['branding']); ?>
-    </div>
-    <div id="navigation">
-      <?php print render($page['navigation']); ?>
-    </div><!-- /.navigation -->
-  </div> <!-- /.container -->
-</header>
-
-<div id="main-wrapper">
-  <div id="main" class="main">
+<div id="body-inner">
+  <header id="header" class="header" role="header">
     <div class="container">
-      <?php if ($messages): ?>
-        <div id="messages">
-          <?php print $messages; ?>
-        </div>
-      <?php endif; ?>
-      <?php if (!empty($page['sidebar_first'])): ?>
-        <aside id="sidebar-first" class="col-sm-3" role="complementary">
-          <?php print render($page['sidebar_first']); ?>
-        </aside>
-      <?php endif; ?>
-      <div id="content-wrapper" <?php if(!empty($page['sidebar_first'])) print 'class="col-sm-9"'; ?>>
-        <div id="page-header">
-          <?php if ($title): ?>
-            <div class="page-header">
-              <h1 class="title"><?php print $title; ?></h1>
+      <div id="top">
+        <nav class="navbar navbar-default">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header top">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
             </div>
-          <?php endif; ?>
-          <?php if ($tabs): ?>
-            <div class="tabs">
-              <?php print render($tabs); ?>
-            </div>
-          <?php endif; ?>
-          <?php if ($action_links): ?>
-            <ul class="action-links">
-              <?php print render($action_links); ?>
-            </ul>
-          <?php endif; ?>
-        </div>
-        <div id="content" class="col-sm-12 <?php print (!$is_panel) ? 'container' : ''; ?>">
-          <?php print render($page['content']); ?>
-        </div>
-      </div><!-- /#content-wrapper -->
-    </div><!-- #container -->
-  </div> <!-- /#main -->
-</div> <!-- /#main-wrapper -->
 
-<footer id="footer" class="footer" role="footer">
-  <div class="container">
-    <?php render($page['footer']); ?>
-  </div>
-</footer>
+            <div class="collapse navbar-collapse">
+              <ul class="nav nav-tabs navbar-left">
+                <li class="dropdown">
+                  <a class="dropdown-toggle" data-toggle="dropdown" href="#"> HR <span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <?php print render($main_menu_dropdown); ?>
+                  </ul>
+                </li>
+                <?php if ($og_group): ?>
+                  <li class="active"><?php print $og_group; ?></li>
+                <?php endif; ?>
+              </ul>
+              <div class="navbar-right">
+                <?php print render($page['top']); ?>
+              </div>
+            </div><!-- .navbar-collapse -->
+        </nav>
+      </div><!-- #top -->
+    </div><!-- .container-fluid -->
+    <div class="container header">
+      <div id="branding">
+          <?php if ($logo): ?>
+            <a href="<?php print $front_page; ?>" id="logo">
+              <img src="<?php print $logo; ?>" alt="Humanitarianresponse Logo" />
+            </a>
+          <?php endif; ?>
+          <?php print render($page['branding']); ?>
+      </div>
+      <div id="navigation">
+        <?php print render($page['navigation']); ?>
+      </div><!-- /.navigation -->
+    </div> <!-- /.container -->
+  </header>
+
+  <div id="main-wrapper">
+    <div id="main" class="main">
+      <div class="container">
+        <?php if ($messages): ?>
+          <div id="messages">
+            <?php print $messages; ?>
+          </div>
+        <?php endif; ?>
+        <?php if (!empty($page['sidebar_first'])): ?>
+          <aside id="sidebar-first" class="col-sm-3" role="complementary">
+            <?php print render($page['sidebar_first']); ?>
+          </aside>
+        <?php endif; ?>
+        <div id="content-wrapper" <?php if(!empty($page['sidebar_first'])) print 'class="col-sm-9"'; ?>>
+          <div id="page-header">
+            <?php if ($title): ?>
+              <div class="page-header">
+                <h1 class="title"><?php print $title; ?></h1>
+              </div>
+            <?php endif; ?>
+            <?php if ($tabs): ?>
+              <div class="tabs">
+                <?php print render($tabs); ?>
+              </div>
+            <?php endif; ?>
+            <?php if ($action_links): ?>
+              <ul class="action-links">
+                <?php print render($action_links); ?>
+              </ul>
+            <?php endif; ?>
+          </div>
+          <div id="content" class="col-sm-12 <?php print (!$is_panel) ? 'container' : ''; ?>">
+            <?php print render($page['content']); ?>
+          </div>
+        </div><!-- /#content-wrapper -->
+      </div><!-- #container -->
+    </div> <!-- /#main -->
+  </div> <!-- /#main-wrapper -->
+
+  <footer id="footer" class="footer" role="footer">
+    <div class="container">
+      <?php render($page['footer']); ?>
+    </div>
+  </footer>
+</div>
