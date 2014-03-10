06b0110d825b89dc0eb8e0da858ff7f36a3615b3
MaximeFC <maxime.gaul@feelandclic.com>
2014-03-10 17:28:14 +0100

    Update

--- templates/view/views-view-unformatted.tpl.php
+++ templates/view/views-view-unformatted.tpl.php
@@ -11,9 +11,15 @@
 	<?php if (!empty($title)): ?>
 	  <h3 class="views-group-title"><?php print $title; ?></h3>
 	<?php endif; ?>
+	<?php if (!empty($title)): ?>
+		<div class="views-group-content">
+	<?php endif; ?>
 	<?php foreach ($rows as $id => $row): ?>
 	  <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
 	    <?php print $row; ?>
 	  </div>
 	<?php endforeach; ?>
+	<?php if (!empty($title)): ?>
+		</div>
+	<?php endif; ?>
 </div>
