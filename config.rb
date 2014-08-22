14794d318ade86f548cb0c39380f15548248ae80
maximeg01 <maxime.gaul@feelandclic.com>
2014-08-22 17:11:56 +0200

    Source maps sass

--- config.rb
+++ config.rb
@@ -33,3 +33,5 @@ line_comments = (environment == :development) ? true : false
 
 # sass debug
 # sass_options = (environment == :development) ? {:debug_info => true} : {:always_update => true}
+
+sass_options = (environment == :development) ? {:sourcemap => true} : {:always_update => true}
